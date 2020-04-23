df380be48f7471f659f24598b2cef304a4e95526
Juliano Acuña <juliano.acunia@hotmail.com>
2020-04-23 20:41:16 -0300

    Add login reducer in combine reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux'
 import productReducer from './productReducer'
+import loginReducer from './loginReducer'
 
 export default combineReducers({
-    products: productReducer
+    products: productReducer,
+    users: loginReducer
 })
