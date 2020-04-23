29ff2efe378c80e02a255c6de50da462da085911
Juliano Acuña <juliano.acunia@hotmail.com>
2020-04-23 20:35:52 -0300

    Is auth users reducer

---
+++ src/redux/reducers/loginReducer.js
@@ -0,0 +1,36 @@
+import {
+    LOGIN_USER_PENDING,
+    LOGIN_USER_SUCCESS,
+    LOGIN_USER_ERROR,
+    IS_AUTH,
+    USER_LOGOUT,
+    ADD_USER_PENDING,
+    ADD_USER_SUCCESS,
+    ADD_USER_ERROR,
+    FETCH_USERS
+} from '../actions/types'
+
+const initialState = {
+    isAuth: false,
+    users: [],
+    name: undefined,
+    error: null,
+    isLoading: false,
+    message: undefined,
+    logged: false,
+    token: '',
+    failedLogin: false
+}
+
+export default function(state = initialState, action) {
+    switch (action.type) {
+        case IS_AUTH:
+        return {
+          ...state,
+          isAuth: action.payload.isAuth,
+          token: action.payload.token
+        }
+        default:
+            return state
+    }
+}
