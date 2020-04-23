1019d1bb8fd2daad8bfc7161aeffa46c8f824860
Juliano Acuña <juliano.acunia@hotmail.com>
2020-04-23 20:44:47 -0300

    Add categorie reducer in combine reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import productReducer from './productReducer'
 import loginReducer from './loginReducer'
+import categorieReducer from './categorieReducer'
 
 export default combineReducers({
     products: productReducer,
-    users: loginReducer
+    users: loginReducer,
+    categories: categorieReducer
 })
