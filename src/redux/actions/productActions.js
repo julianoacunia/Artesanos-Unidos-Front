fe10c2e7f36d3f42744ef371dccb7459389c13be
Juliano Acuña <juliano.acunia@hotmail.com>
2020-04-23 20:12:09 -0300

    Post products action

--- src/redux/actions/productActions.js
+++ src/redux/actions/productActions.js
@@ -22,3 +22,45 @@ import {
             return dispatch ({ type: FETCH_PRODUCTS, payload: data})
         })
 }
+
+// POST PRODUCTS 
+export const postProduct = product => {
+    return dispatch => {
+        dispatch({
+            type: ADD_PRODUCT_PENDING
+        })
+        const {
+            users: { token }
+        } = store.getState()
+        const options = {
+            timeout: 25000,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `BEARER ${token}`
+            },
+            body: JSON.stringify(product)
+        }
+        console.log('options', options)
+        return fetch('http://localhost:5000/api/products', options)
+            .then( res => res.json())
+            .then( data => {
+                console.log('POST PRODUCT', data)
+                if (!Object.entries(data).length) {
+                    return Promise.reject(data)
+                }
+                return dispatch({
+                    type: ADD_PRODUCT_SUCCESS,
+                    payload: {
+                        product: data
+                    }
+                })
+            })
+            .catch( error => {
+                return dispatch ({
+                    type: ADD_PRODUCT_ERROR,
+                    payload: error
+                })
+            })
+    }
+}
