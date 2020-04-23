d1fed644dfe01cb384124ba580743d53241d186f
Juliano Acuña <juliano.acunia@hotmail.com>
2020-04-23 20:26:40 -0300

    Post categories action

--- src/redux/actions/categorieActions.js
+++ src/redux/actions/categorieActions.js
@@ -17,3 +17,45 @@ export const fetchCategories = () => dispatch => {
             return dispatch ({ type: FETCH_CATEGORIES, payload: data})
         })
 }
+
+// POST CATEGORIES 
+export const postCategories = categorie => {
+    return dispatch => {
+        dispatch({
+            type: ADD_CATEGORIE_PENDING
+        })
+        const {
+            users: { token }
+        } = store.getState()
+        const options = {
+            timeout: 25000,
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `BEARER ${token}`
+            },
+            body: JSON.stringify(categorie)
+        }
+        console.log('options', options)
+        return fetch('http://localhost:5000/api/categories', options)
+            .then( res => res.json())
+            .then( data => {
+                console.log('POST CATEGORIE', data)
+                if (!Object.entries(data).length) {
+                    return Promise.reject(data)
+                }
+                return dispatch({
+                    type: ADD_CATEGORIE_SUCCESS,
+                    payload: {
+                        categorie: data
+                    }
+                })
+            })
+            .catch( error => {
+                return dispatch ({
+                    type: ADD_CATEGORIE_ERROR,
+                    payload: error
+                })
+            })
+    }
+}
