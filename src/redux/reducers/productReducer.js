9ca7680e2f146baf8ed4c38f99fa7b42abdbe6c8
Juliano Acuña <juliano.acunia@hotmail.com>
2020-04-23 20:33:15 -0300

    Order products reducer

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -104,6 +104,13 @@ export default function ( state = initialState, action) {
               productSelected: action.payload
             }
         }
+        case ORDER_PRODUCTS_BY_PRICE: {
+            return {
+              ...state,
+              sort: action.payload.sort,
+              items: action.payload.items
+            }
+        }
         default:
         return state
     }
