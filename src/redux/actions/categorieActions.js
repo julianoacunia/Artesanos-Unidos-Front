0d9c35c221d55612123ccce7795e8ddffdacc253
Juliano Acuña <juliano.acunia@hotmail.com>
2020-04-23 20:26:15 -0300

    Fetch categories action

---
+++ src/redux/actions/categorieActions.js
@@ -0,0 +1,19 @@
+import {
+    FETCH_CATEGORIES,
+    ADD_CATEGORIE_PENDING,
+    ADD_CATEGORIE_SUCCESS,
+    ADD_CATEGORIE_ERROR,
+    DELETE_CATEGORIE_PENDING,
+    DELETE_CATEGORIE_SUCCESS,
+    DELETE_CATEGORIE_ERROR
+} from './types'
+import store from '../store'
+
+// FETCH ACTIONS
+export const fetchCategories = () => dispatch => {
+    fetch('http://localhost:5000/api/categories')
+        .then(res => res.json())
+        .then(data => {
+            return dispatch ({ type: FETCH_CATEGORIES, payload: data})
+        })
+}
