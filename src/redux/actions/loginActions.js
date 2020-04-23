f191fa4058baf36f4737e01b555a8d45831fa569
Juliano Acuña <juliano.acunia@hotmail.com>
2020-04-23 20:24:04 -0300

    Post users action

--- src/redux/actions/loginActions.js
+++ src/redux/actions/loginActions.js
@@ -70,3 +70,40 @@ export const fetchUser = () => dispatch => {
     })
 }
 
+//POST USERS
+export const postUser = user => {
+    return dispatch => {
+      dispatch({
+        type: ADD_USER_PENDING
+      })
+      const options = {
+        timeout: 25000,
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+      }
+      console.log('options', options)
+      return fetch(`http://localhost:5000/api/users`, options)
+        .then(res => res.json())
+        .then(data => {
+          console.log('POST USER', data)
+          if (!Object.entries(data).length) {
+            return Promise.reject(data)
+          }
+          return dispatch({
+            type: ADD_USER_SUCCESS,
+            payload: {
+              user: data
+            }
+          })
+        })
+        .catch(error => {
+          return dispatch({
+            type: ADD_USER_ERROR,
+            payload: error
+          })
+        })
+    }
+}
